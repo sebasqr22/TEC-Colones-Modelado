ac54f9ae8833080836d2f7a1776a5b236c10defe
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-04-28 12:19:19 -0600

    Inicio de logica en la pagina

--- tec_colones/src/app/vista-admin/vista-admin.component.ts
+++ tec_colones/src/app/vista-admin/vista-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, Renderer2, ElementRef, QueryList, OnInit } from '@angular/core';
 import {DatabaseService} from "../database.service";
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
@@ -9,6 +9,8 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./vista-admin.component.css']
 })
 export class VistaAdminComponent {
+  busqueda = true;
+  @ViewChildren('cambio_color') elementos: QueryList<ElementRef> | undefined;
   generarMaterial = this.fb.group({
     nombre: ['', Validators.required],
     unidad: ['', Validators.required],
@@ -16,7 +18,23 @@ export class VistaAdminComponent {
     estado: ['', Validators.required],
     descripcion: ['', Validators.required]
   });
-  crearMaterial(){
+
+
+  pantallas = ['crearMaterial'];
+  pantallaActual = 'crearMaterial';
+
+  cambiarPantalla(pantalla:string){
+    let actual = document.getElementById(this.pantallaActual);
+    // @ts-ignore
+    actual.style.display = 'none';
+
+    this.pantallaActual = pantalla;
+    actual = document.getElementById(this.pantallaActual);
+    // @ts-ignore
+    actual.style.display = 'block';
+  }
+
+  crearMaterial() {
     let primary;
     if (this.generarMaterial.valid) {
       primary = this.base.generateCode()
@@ -36,7 +54,45 @@ export class VistaAdminComponent {
 
   }
 
+  pantallaBusqueda(){
+    this.busqueda = !this.busqueda;
+    const todo = document.getElementById('todo');
+    if(this.busqueda){
+      // @ts-ignore
+      todo.className = '';
+    }
+    else{
+      // @ts-ignore
+      todo.className = 'sb-sidenav-toggled';
+    }
+  }
 
-  constructor(private base: DatabaseService, private fb: FormBuilder){
+
+  cambiarColor(color:string) {
+    // @ts-ignore
+    this.elementos.forEach(elemento => {
+      this.renderer.setStyle(elemento.nativeElement, 'color', color);
+    });
+  }
+
+  esconderTodas(){
+    let actual ;
+    this.pantallas.forEach(pantalla => {
+      actual = document.getElementById(pantalla);
+      // @ts-ignore
+      actual.style.display = 'none';
+    })
+    actual = document.getElementById(this.pantallaActual);
+    // @ts-ignore
+    actual.style.display = 'block';
+  }
+
+
+
+
+  constructor(private base: DatabaseService, private fb: FormBuilder, private renderer: Renderer2){}
+
+  ngOnInit():void{
+    this.esconderTodas();
   }
 }
