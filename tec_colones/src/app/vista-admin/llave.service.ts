6acd7830ffded4257f181fb73990edf25a45b656
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-05-11 18:28:24 -0600

    Arreglos
    Co-Authored-By: Marco Alonso Rodríguez Villegas <67651562+markorovi@users.noreply.github.com>

--- tec_colones/src/app/vista-admin/llave.service.ts
+++ tec_colones/src/app/vista-admin/llave.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {DatabaseService} from "./database.service";
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,61 @@ export class LlaveService {
 
   private readonly characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+  llaves:any[] = [];
+
+  validarExistentes(identificador:string){
+
+    let iden = "";
+
+    switch (identificador){
+      case 'S':
+        iden = 'sedes';
+        break;
+
+      case 'M':
+        iden = 'material'
+        break;
+
+
+      default:
+        break;
+    }
+
+    this.llaves = [];
+
+    this.base.get(iden).then((value => {
+      const elementos = JSON.parse(JSON.stringify(value));
+      for (let clave in elementos) {
+        if (elementos.hasOwnProperty(clave)) {
+          const elemento = elementos[clave];
+          // @ts-ignore
+          this.llaves.push(elemento.pk);
+        }
+      }
+
+
+    }));
+  }
+
   generateCode(llave:string): string {
     const length = 12
     let result = '';
     const charactersLength = this.characters.length;
-    for (let i = 0; i < length; i++) {
-      result += this.characters.charAt(Math.floor(Math.random() * charactersLength));
+
+    this.validarExistentes(llave);
+
+    while(true){
+      for (let i = 0; i < length; i++) {
+        result += this.characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+
+      if(!this.llaves.includes(result)){
+        break;
+      }
     }
+
     return llave + '-' + result;
   }
 
-  constructor() { }
+  constructor(private base: DatabaseService) { }
 }
