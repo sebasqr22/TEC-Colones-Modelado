f51fcf3443033bdd56f076375d7a1e87f2ec9126
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-05-29 17:52:39 -0600

    Cambios

--- tec_colones/src/app/vista-admin/asignacion-tec-colones/asignacion-tec-colones.component.ts
+++ tec_colones/src/app/vista-admin/asignacion-tec-colones/asignacion-tec-colones.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-asignacion-tec-colones',
   templateUrl: './asignacion-tec-colones.component.html',
   styleUrls: ['./asignacion-tec-colones.component.css']
 })
-export class AsignacionTecColonesComponent {
+export class AsignacionTecColonesComponent implements OnInit{
+
+
+
+  constructor(){}
+
+  ngOnInit() {
+
+  }
 }
